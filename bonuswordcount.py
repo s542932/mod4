ef3a59eeeb49cc48bba4a3a89e22d2b482a96f36
s542932 <84051396+s542932@users.noreply.github.com>
2021-06-06 14:14:42 -0500

    Create bonuswordcount.py

---
+++ bonuswordcount.py
@@ -0,0 +1,17 @@
+with open('module4bonustext') as f:
+    lines = f.readlines()
+
+word_counts = {}
+
+for word in text.split():
+    if word in word_counts:
+        word_counts[word] += 1
+    else:
+        word_counts[word] = 1
+        
+print(f'{"WORD":<12}COUNT')
+ 
+for word, count in sorted(word_counts.items()):
+    print(f'{word:<12}{count}')
+     
+print('nNumber of Unique words:', len(word_counts))
